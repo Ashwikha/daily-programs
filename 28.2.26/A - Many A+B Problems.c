b783baffa298019c759e6bd7f7dd9e0a3343e24a
Ashwikha <ashwikhas@gmail.com>
2026-02-28 21:41:34 +0530

    Restore A+B problem code with comments
    Restored the original code functionality and added comments explaining the problem statement and constraints.

--- 28.2.26/A - Many A+B Problems.c
+++ 28.2.26/A - Many A+B Problems.c
@@ -1,3 +1,136 @@
+/* You are given 
+N pairs of integers: 
+(A 
+1
+​
+ ,B 
+1
+​
+ ),(A 
+2
+​
+ ,B 
+2
+​
+ ),…,(A 
+N
+​
+ ,B 
+N
+​
+ ). For each 
+i=1,2,…,N, print 
+A 
+i
+​
+ +B 
+i
+​
+ .
+
+Constraints
+1≤N≤1000
+−10 
+9
+ ≤A 
+i
+​
+ ,B 
+i
+​
+ ≤10 
+9
+ 
+All values in the input are integers.
+Input
+The input is given from Standard Input in the following format:
+
+N
+A 
+1
+​
+  
+B 
+1
+​
+ 
+A 
+2
+​
+  
+B 
+2
+​
+ 
+⋮
+A 
+N
+​
+  
+B 
+N
+​
+ 
+Output
+Print 
+N lines. For 
+i=1,2,…,N, the 
+i-th line should contain 
+A 
+i
+​
+ +B 
+i
+​
+ .
+
+Sample Input 1
+
+4
+3 5
+2 -6
+-5 0
+314159265 123456789
+Sample Output 1
+
+8
+-4
+-5
+437616054
+The first line should contain 
+A 
+1
+​
+ +B 
+1
+​
+ =3+5=8.
+The second line should contain 
+A 
+2
+​
+ +B 
+2
+​
+ =2+(−6)=−4.
+The third line should contain 
+A 
+3
+​
+ +B 
+3
+​
+ =(−5)+0=−5.
+The fourth line should contain 
+A 
+4
+​
+ +B 
+4
+​
+ =314159265+123456789=437616054.  */
+
+
 #include<stdio.h>
 int main()
 {
@@ -11,4 +144,5 @@ int main()
     printf("%lld\n",a+b);
   }
   return 0;
+
 }
