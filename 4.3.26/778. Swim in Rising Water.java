a0dc60cc5422795f2f960056ad1ce595568906ca
Ashwikha <ashwikhas@gmail.com>
2026-03-04 22:58:12 +0530

    Add files via upload

---
+++ 4.3.26/778. Swim in Rising Water.java
@@ -0,0 +1,75 @@
+/*
+You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+
+It starts raining, and water gradually rises over time. At time t, the water level is t, meaning any cell with elevation less than equal to t is submerged or reachable.
+
+You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+Return the minimum time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,2],[1,3]]
+Output: 3
+Explanation:
+At time 0, you are in grid location (0, 0).
+You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+You cannot reach point (1, 1) until time 3.
+When the depth of water is 3, we can swim anywhere inside the grid.
+Example 2:
+
+
+Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+Output: 16
+Explanation: The final route is shown.
+We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ 
+
+Constraints:
+
+n == grid.length
+n == grid[i].length
+1 <= n <= 50
+0 <= grid[i][j] < n2
+Each value grid[i][j] is unique.
+*/
+
+
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int R=grid.length;
+        int C=grid[0].length;
+        boolean vis[][]=new boolean[R][C];
+        PriorityQueue<int[]> minht=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        minht.add(new int[]{grid[0][0],0,0});
+        vis[0][0]=true;
+        int maxht=grid[0][0];
+        int diff[]={0,1,0,-1,0};
+        while(!minht.isEmpty()){
+            int cell[]=minht.poll();
+            maxht=Math.max(maxht,cell[0]);
+            int row=cell[1],col=cell[2];
+            for(int i=0;i<4;i++)
+            {
+                int ar=row+diff[i];
+                int ac=col+diff[i+1];
+                if(ar<R && ar>=0 && ac<C && ac>=0)
+                {
+                    if(ar==R-1 && ac==C-1){
+                        return Math.max(maxht,grid[ar][ac]); 
+                       
+                    }
+                    if(!vis[ar][ac])
+                    {
+                     minht.add(new int[]{grid[ar][ac],ar,ac});
+                     vis[ar][ac]=true;
+                    }
+                }         
+        }
+    }
+    return maxht;   //not needed for compiler to work we use
+    }
+} 
---
+++ 4.3.26/B. Deletion Sort codeforces.cpp
@@ -0,0 +1,97 @@
+/*
+AksLolCoding is playing a game on an array a
+ of n
+ positive integers. During each turn:
+
+If a
+ is non-decreasing∗
+, the game ends.
+Otherwise, AksLolCoding can choose any single element and remove it from the array.
+Determine the minimum possible number of elements that can be remaining in the array after the game ends.
+
+∗
+a
+ is non-decreasing if ai≤ai+1
+ for all 1≤i≤m−1
+, where m
+ is the length of a
+.
+
+Input
+The first line contains an integer t
+ (1≤t≤1000
+), the number of test cases.
+
+The first line of each test case contains an integer n
+ (1≤n≤10
+).
+
+The second line of each test case contains n
+ integers, the elements of a
+ (1≤ai≤100
+).
+
+Output
+For each test case, output an integer: the minimum possible number of elements left once the array is sorted.
+
+Example
+Input
+3
+4
+1 4 2 3
+1
+100
+2
+6 7
+Output
+1
+1
+2
+Note
+In the first test case, the minimum of 1
+ element can be achieved by removing 1
+, 2
+, and 3
+ in that order.
+
+In the second and third test cases, no elements can be removed.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+#define ll long long int
+
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    bool dec = false;
+
+    for (int i = 1; i < N; i++) {
+        if (arr[i] < arr[i - 1]) {
+            dec = true;
+            break;
+        }
+    }
+
+    cout << (dec ? 1 : N) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+        solve();
+}
