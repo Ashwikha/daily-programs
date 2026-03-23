c7eeed094af6b8a2f0a2bff3d63a8e9dd641efac
Ashwikha <ashwikhas@gmail.com>
2026-03-23 20:49:26 +0530

    Add files via upload

---
+++ 23.3.26/A - box atcoder.cpp
@@ -0,0 +1,53 @@
+/*
+We have removed 
+A balls from a box that contained 
+N balls and then put 
+B new balls into that box. How many balls does the box contain now?
+
+Constraints
+All values in input are integers.
+100≤N≤200
+1≤A,B≤100
+Input
+Input is given from Standard Input in the following format:
+
+N 
+A 
+B
+Output
+Print the answer as an integer.
+
+Sample Input 1
+Copy
+100 1 2
+Sample Output 1
+Copy
+101
+The box contained 
+100 balls. After removing 
+1 ball and putting 
+2 new balls, it now contains 
+101 balls.
+
+Sample Input 2
+Copy
+100 2 1
+Sample Output 2
+Copy
+99
+Sample Input 3
+Copy
+100 1 1
+Sample Output 3
+Copy
+100
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int x,a,b;
+  cin>>x>>a>>b;
+  cout<<x-a+b;
+  return 0;
+}
