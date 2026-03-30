bd6782b3c04c9c7dbcb5a839ee9c4da2414a8ce5
Ashwikha <ashwikhas@gmail.com>
2026-03-30 19:45:08 +0530

    Add files via upload

---
+++ 30.3.26/22. Generate Parentheses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+*/
+
+public:
+    void recursive(int n, int op, int cl, string temp, vector<string>& ans) {
+        if (temp.length() == (2 * n)) {
+            ans.push_back(temp);
+        }
+        if (op < n)
+            recursive(n, op + 1, cl, temp + '(', ans);
+
+        if (cl < op)
+
+            recursive(n, op, cl + 1, temp + ')', ans);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp = "";
+        int op = 0;
+        int cl = 0;
+        recursive(n, op, cl, temp, ans);
+        return ans;
+    }
+};
