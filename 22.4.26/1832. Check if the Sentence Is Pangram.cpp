3f3ccf8610eb4f0b8d24ec0fb0649317efd929dd
Ashwikha <ashwikhas@gmail.com>
2026-04-22 18:19:42 +0530

    Add files via upload

---
+++ 22.4.26/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,40 @@
+/*
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+ 
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+ 
+*/
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int fre[26]={0};
+        for(int i=0;i<sentence.size();i++)
+        {
+            fre[sentence[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(fre[i]<1)
+            return 0;
+        }
+        return 1;
+    }
+};
