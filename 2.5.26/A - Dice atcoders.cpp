911321f725bea71ac04079d38c42db3222c78ef9
Ashwikha <ashwikhas@gmail.com>
2026-05-02 19:45:33 +0530

    Add files via upload

---
+++ 2.5.26/A - Dice atcoders.cpp
@@ -0,0 +1,56 @@
+/*
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+  int x;
+  cin>>x;
+  if(x>2 && x<19)
+  {
+    cout<<"Yes";
+  }
+  else
+  {
+    cout<<"No";
+  }
+  return 0;
+}There are three dice, each with six faces.
+Each die has 
+1,2,3,4,5,6 written on its faces.
+
+When these dice are rolled simultaneously, is it possible for the sum of the rolled values to be 
+X?
+
+Constraints
+X is an integer between 
+1 and 
+20, inclusive.
+Input
+The input is given from Standard Input in the following format:
+
+X
+Output
+If it is possible for the sum of the rolled values to be 
+X, output Yes; otherwise, output No.
+
+Sample Input 1
+Copy
+15
+Sample Output 1
+Copy
+Yes
+For example, if the rolled values are 
+4,5,6, the sum is 
+15.
+
+Sample Input 2
+Copy
+2
+Sample Output 2
+Copy
+No
+
+*/
