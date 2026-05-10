125fdc138437601477d2520042b34c46c84c4cbe
Ashwikha <ashwikhas@gmail.com>
2026-05-10 17:05:11 +0530

    Add files via upload

---
+++ 10.5.26/3921. Score Validator.cpp
@@ -0,0 +1,96 @@
+/*
+You are given a string array events.
+
+Initially, score = 0 and counter = 0. Each element in events is one of the following:
+
+"0", "1", "2", "3", "4", "6": Add that value to the total score.
+"W": Increase the counter by 1. No score is added.
+"WD": Add 1 to the total score.
+"NB": Add 1 to the total score.
+Process the array from left to right. Stop processing when either:
+
+All elements in events have been processed, or
+The counter becomes 10.
+Return an integer array [score, counter], where:
+
+score is the final total score.
+counter is the final counter value.
+ 
+
+Example 1:
+
+Input: events = ["1","4","W","6","WD"]
+
+Output: [12,1]
+
+Explanation:
+
+Event	Score	Counter
+"1"	1	0
+"4"	5	0
+"W"	5	1
+"6"	11	1
+"WD"	12	1
+Final result: [12, 1].
+
+Example 2:
+
+Input: events = ["WD","NB","0","4","4"]
+
+Output: [10,0]
+
+Explanation:
+
+Event	Score	Counter
+"WD"	1	0
+"NB"	2	0
+"0"	2	0
+"4"	6	0
+"4"	10	0
+Final result: [10, 0].
+
+Example 3:
+
+Input: events = ["W","W","W","W","W","W","W","W","W","W","W"]
+
+Output: [0,10]
+
+Explanation:
+
+After 10 occurrences of "W", the counter reaches 10, so processing stops. The remaining events are ignored.
+
+ 
+
+Constraints:
+
+1 <= events.length <= 1000
+events[i] is one of "0", "1", "2", "3", "4", "6", "W", "WD", or "NB".©leetcode
+*/
+
+
+class Solution {
+public:
+    vector<int> scoreValidator(vector<string>& events) {
+        int s=0,c=0;
+        for(int i=0;i<events.size();i++){
+            if(c==10){
+                    return {s,c};
+            }
+            if(events[i]>="0" && events[i]<="9"){
+                s+=stoi(events[i]);
+            }
+            else if(events[i]=="W"){
+            
+        
+                c+=1;
+            }
+            else if(events[i]=="WD"){
+                s+=1;
+            }
+            else if(events[i]=="NB"){
+                s+=1;
+            }
+        }
+        return {s,c};
+    }
+};©leetcode
